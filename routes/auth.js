a53f1185c77424947e43d79e0af1362e2fd5a05b
mookt <kilian.poullet@gmail.com>
2026-05-25 17:57:50 +0200

    Replace bcrypt with bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 const express  = require('express');
-const bcrypt   = require('bcrypt');
+const bcrypt   = require('bcryptjs');
 const jwt      = require('jsonwebtoken');
 const { rateLimiter }            = require('../middleware/rateLimiter');
 const { envoyerCodeVerification } = require('../services/email');
